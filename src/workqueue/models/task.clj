8bd00e7867b24e64fa3678467a3168ba27b1fb83
Ron Toland <rontoland@gmail.com>
2013-02-02 11:30:22 -0800

    Add model for tasks. Update server file to connect to mongodb on launch

---
+++ src/workqueue/models/task.clj
@@ -0,0 +1,42 @@
+(ns workqueue.models.task
+  (:use monger.operators)
+  (:require [monger.core :as mg]
+            [monger.collection :as mc])
+  (:import com.mongodb.WriteConcern [org.bson.types ObjectId]))
+
+(defn get-task-count
+  "Get the number of tasks in the user's queue"
+  [user-id]
+  (mc/count "tasks" {:user_id user-id}))
+
+(defn generate-task-id
+  "Get the next highest task id for the user"
+  [user-id]
+  (let [task-count (get-task-count user-id)]
+    (+ task-count 1)))
+
+(defn add-task
+  "Add a task to the user's queue"
+  [user-id text]
+  (let [task {:user_id user-id :task text :task_id (generate-task-id user-id) :_id (ObjectId.)}]
+   (mc/insert-and-return "tasks" task)))
+
+(defn delete-task
+  "Remove a task from the user's queue"
+  [user-id task-id]
+  (mc/remove "tasks" {:task_id task-id :user_id user-id}))
+
+(defn get-task
+  "Fetch a single task from the user's queue"
+  [user-id task-id]
+  (mc/find-one-as-map "tasks" {:task_id task-id :user_id user-id}))
+
+(defn get-queue
+  "Fetch all the user's tasks"
+  [user-id]
+  (mc/find-maps "tasks" {:user_id user-id}))
+
+(defn update-task
+  "Update a given task"
+  [user-id task-id text]
+  (mc/update "tasks" {:user_id user-id :task_id task-id} {$set {:task text}}))
--- src/workqueue/server.clj
+++ src/workqueue/server.clj
@@ -4,7 +4,13 @@
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [compojure.response :as response]))
+            [compojure.response :as response]
+            [monger.core :as mg]))
+
+(def mongo-host (get (System/getenv)
+                     "MONGOLAB_URI" "mongodb://127.0.0.1/workqueue"))
+
+(mg/connect-via-uri! mongo-host)
 
 (defroutes app-routes
   (GET "/" [] (dashboard))
