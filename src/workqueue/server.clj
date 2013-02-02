025c02015a3a3745f86204e23e2e2c722922e4a5
Ron Toland <rontoland@gmail.com>
2013-02-02 09:43:23 -0800

    Remove old noir-dependent files and structure

--- src/workqueue/server.clj
+++ src/workqueue/server.clj
@@ -1,11 +1,3 @@
-(ns workqueue.server
-  (:require [noir.server :as server]))
+(ns workqueue.server)
 
-(server/load-views "src/workqueue/views/")
-
-(defn -main [& m]
-  (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (server/start port {:mode mode
-                        :ns 'workqueue})))
 
