89dbf20a77bd1c798d9b1172a49b32fc67bd3f86
Ron Toland <rontoland@gmail.com>
2013-02-02 09:40:25 -0800

    Remove noir dependence

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
-(defproject workqueue "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+(defproject workqueue "0.2.0"
+            :description "A GTD approach to project management"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.2"]
+                           [compojure "1.1.3"]
+                           [lib-noir "0.2.0"]
                            [com.novemberain/monger "1.3.4"]]
-            :main workqueue.server)
+            :plugins [[lein-ring "0.7.1"]]
+            :ring {:handler workqueue.server/handler
+                   :auto-reload? false})
 
